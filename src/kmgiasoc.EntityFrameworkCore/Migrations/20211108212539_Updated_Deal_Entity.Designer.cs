2555f2086773cda34a921925e4bf2760e945e7be
VietVB-M3800 <funaway89@gmail.com>
2021-11-08 22:37:08 +0100

    update Deal

--- src/kmgiasoc.EntityFrameworkCore/Migrations/20211108212539_Updated_Deal_Entity.Designer.cs
+++ src/kmgiasoc.EntityFrameworkCore/Migrations/20211108212539_Updated_Deal_Entity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 using kmgiasoc.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using kmgiasoc.EntityFrameworkCore;
 namespace kmgiasoc.Migrations
 {
     [DbContext(typeof(kmgiasocDbContext))]
-    partial class kmgiasocDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211108212539_Updated_Deal_Entity")]
+    partial class Updated_Deal_Entity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
