d5ca21d19a97fb79d5b3bd72f43f0e0d327d25f4
VietVB-M3800 <funaway89@gmail.com>
2021-11-15 23:48:22 +0100

    update context

--- src/kmgiasoc.EntityFrameworkCore/Migrations/20211115224702_Updated_Deal_Entity_v20211115.Designer.cs
+++ src/kmgiasoc.EntityFrameworkCore/Migrations/20211115224702_Updated_Deal_Entity_v20211115.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 using kmgiasoc.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using kmgiasoc.EntityFrameworkCore;
 namespace kmgiasoc.Migrations
 {
     [DbContext(typeof(kmgiasocDbContext))]
-    partial class kmgiasocDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211115224702_Updated_Deal_Entity_v20211115")]
+    partial class Updated_Deal_Entity_v20211115
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/kmgiasoc.EntityFrameworkCore/Migrations/20211115224702_Updated_Deal_Entity_v20211115.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace kmgiasoc.Migrations
+{
+    public partial class Updated_Deal_Entity_v20211115 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeatured",
+                table: "AppDeals",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "AppDeals",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFeatured",
+                table: "AppDeals");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "AppDeals");
+        }
+    }
+}
